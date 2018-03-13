27b3cb0a9e86ab72a99a20624b130c5f86226f86
Alex <djdoktop@gmail.com>
2018-03-13 19:56:22 +0300

    1 unsorted array. Why?

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ module.exports = function longestConsecutiveLength(array) {
   // your solution here
     let length = 0;
     let count = 0;
-    array.sort();
+    array.sort((a,b) => a-b);
     if(array.length<2) return array.length;
     array.forEach(function (item, i) {
         if(item === array[i+1]-1){
-            count ++;
-        }else{
+            ++count;
+        }else if(item !== array[i+1]){
             count=0;
         }
         if(length<count) length = count;
     });
-    return length+1;
+    return ++length;
 };
